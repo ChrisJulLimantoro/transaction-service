f5b2a877587e1cbdd42186bd9b22d88367ac025b
leonardo <nickholasleonardo@gmail.com>
2025-01-27 00:37:00 +0700

    feat: done sync crud voucher

--- src/voucher/voucher.controller.ts
+++ src/voucher/voucher.controller.ts
@@ -34,4 +34,58 @@ export class VoucherController {
       };
     }
   }
+
+  @MessagePattern({ cmd: 'put:voucher/*' })
+  @Describe('Update Voucher')
+  async update(@Payload() data: any): Promise<any> {
+    console.log(data);
+    const param = data.params;
+    const body = data.body;
+    try {
+      const result = await this.voucherService.update(param.id, body);
+      this.marketplaceClient.emit(
+        { module: 'voucher', action: 'update' },
+        result,
+      );
+      return {
+        success: true,
+        message: 'Success Update Voucher!',
+        data: result,
+        statusCode: 200,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: error.message,
+        errors: [error.message],
+        statusCode: 500,
+      };
+    }
+  }
+
+  @MessagePattern({ cmd: 'delete:voucher/*' })
+  @Describe('Soft Delete Voucher')
+  async delete(@Payload() data: any): Promise<any> {
+    const param = data.params;
+    try {
+      const result = await this.voucherService.softDelete(param.id);
+      this.marketplaceClient.emit(
+        { module: 'voucher', action: 'delete' },
+        result,
+      );
+      return {
+        success: true,
+        message: 'Success Delete Voucher!',
+        data: result,
+        statusCode: 200,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: error.message,
+        errors: [error.message],
+        statusCode: 500,
+      };
+    }
+  }
 }
--- src/voucher/voucher.service.ts
+++ src/voucher/voucher.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ValidationService } from 'src/validation/validation.service';
 import { VoucherRequest } from './voucher.model';
 import { VoucherValidation } from './voucher.validation';
 import { Decimal } from '@prisma/client/runtime/library';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class VoucherService {
@@ -38,5 +39,75 @@ export class VoucherService {
     });
 
     return createdVoucher;
+  }
+
+  async update(voucher_id: string, request: VoucherRequest): Promise<any> {
+    VoucherValidation.CREATE.parse(request);
+    const voucher = await this.prisma.voucher.findUnique({
+      where: { id: voucher_id },
+      include: { store: true },
+    });
+
+    if (!voucher) {
+      throw new HttpException(
+        `Voucher with ID ${voucher_id} not found`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    const updatedVoucher = await this.prisma.voucher.update({
+      where: { id: voucher_id },
+      data: {
+        name: request.voucher_name,
+        code: 'codetes',
+        discount_amount: new Decimal(request.discount_amount),
+        max_discount: new Decimal(request.max_discount),
+        min_purchase: new Decimal(request.minimum_purchase),
+        poin_price: request.poin_price,
+        description: request.description,
+        start_date: new Date(request.start_date),
+        end_date: new Date(request.end_date),
+        is_active: request.is_active,
+        store_id: request.store_id,
+      },
+      include: {
+        store: true,
+      },
+    });
+
+    return updatedVoucher;
+  }
+
+  async softDelete(voucher_id: string): Promise<any> {
+    const voucher = await this.prisma.voucher.findUnique({
+      where: { id: voucher_id },
+      include: { store: true },
+    });
+
+    if (!voucher) {
+      throw new HttpException(
+        `Voucher with ID ${voucher_id} not found`,
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    if (voucher.deleted_at) {
+      throw new HttpException(
+        `Voucher with ID ${voucher_id} is already deleted`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const deletedVoucher = await this.prisma.voucher.update({
+      where: { id: voucher_id },
+      data: {
+        deleted_at: new Date(), // Mark as deleted
+      },
+      include: {
+        store: true,
+      },
+    });
+
+    return deletedVoucher;
   }
 }
